e2036e1af75c55a74af9ff0629f2c099c29d16dc
unknown <pauazap@gmail.com>
2021-01-30 02:35:58 +0100

    Added MapGeneration base script

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int _columns = 320;
+    public int _rows = 320;
+
+    public GameObject _externalSprite;
+    public GameObject _floorSprite;
+    public GameObject _playerSprite;
+
+    void Start() {
+        GenerateMap();
+        SpawnPlayer();
+    }
+
+    private void GenerateMap() {
+        for(int i = 0;i < _columns;i++) {
+            for(int j = 0;j < _rows;j++) {
+                GameObject selectedTile;
+                if(i == 0 || i == _columns - 1 || j == 0 || j == _rows - 1) {
+                    selectedTile = _externalSprite;
+                } else {
+                    selectedTile = _floorSprite;
+                }
+                Instantiate(selectedTile,new Vector3(i,j,0.0f),Quaternion.identity);
+            }
+        }
+    }
+
+    private void SpawnPlayer() {
+        if (_playerSprite != null) {
+            Instantiate(_playerSprite, new Vector3(1, 1, 0.0f), Quaternion.identity);
+        }
+    }
+}
